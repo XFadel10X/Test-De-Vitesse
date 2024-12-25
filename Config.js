4e444e51d16c05dfd60aaf37cd5d1d5c1f876a31
XFadel10X <assanifadel10@gmail.comm>
2024-12-25 19:10:35 +0100

    patch

--- Config.js
+++ Config.js
@@ -1,17 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBnHFrUxXFnJusq9btIfrCBOHXI628YVEs",
-    authDomain: "speed-test-266e0.firebaseapp.com",
-    databaseURL: "https://speed-test-266e0-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "speed-test-266e0",
-    storageBucket: "speed-test-266e0.firebasestorage.app",
-    messagingSenderId: "806407685056",
-    appId: "1:806407685056:web:66239d8b1b782331363e25",
-    measurementId: "G-0927KS41EB"
-  };
+  apiKey: "AIzaSyBnHFrUxXFnJusq9btIfrCBOHXI628YVEs",
+  authDomain: "speed-test-266e0.firebaseapp.com",
+  databaseURL: "https://speed-test-266e0-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "speed-test-266e0",
+  storageBucket: "speed-test-266e0.firebasestorage.app",
+  messagingSenderId: "806407685056",
+  appId: "1:806407685056:web:66239d8b1b782331363e25",
+  measurementId: "G-0927KS41EB"
+};
 
 
 const app = initializeApp(firebaseConfig);
